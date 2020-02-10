396a4fe431b41e2aaf4a3bb21ae15f196b12667d
Gabriel Rocha <knuckles4x@gmail.com>
2020-02-09 22:31:10 -0300

    salvando a lista de todos no local storage

--- todos.js
+++ todos.js
@@ -2,11 +2,7 @@ var listElement = document.querySelector('#app ul');
 var inputElement = document.querySelector('#app input');
 var buttonElemement = document.querySelector('#app button');
 
-var todos = [
-    'Fazer cafe',
-    'Estudar Javascript',
-    'Acessar comunidade da Rocketseat'
-];
+var todos = JSON.parse(localStorage.getItem('list_todos')) || '';
 
 function renderTodos() {
     listElement.innerHTML = '';
@@ -33,11 +29,17 @@ function addTodo() {
     todos.push(todoText);
     inputElement.value = '';
     renderTodos();
+    saveToStorage();
 }
 
 function deleteTodo(pos){
     todos.splice(pos, 1);
     renderTodos();
+    saveToStorage();
+}
+
+function saveToStorage(){
+    localStorage.setItem('list_todos', JSON.stringify(todos));
 }
 
 buttonElemement.onclick = addTodo;
