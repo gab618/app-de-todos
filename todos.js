d406a6baa36792f54c2094a096d21bdf85350896
Gabriel Rocha <knuckles4x@gmail.com>
2020-02-09 22:05:06 -0300

    excluindo todos

--- todos.js
+++ todos.js
@@ -14,8 +14,17 @@ function renderTodos() {
         var todoElement = document.createElement('li');
         var todoText = document.createTextNode(todo);
 
+        var linkElement = document.createElement('a');
+        linkElement.setAttribute('href', '#');
+        pos = todos.indexOf(todo);
+        linkElement.setAttribute('onclick','deleteTodo('+pos+')');
+        var linkText = document.createTextNode('Excluir');
+        
+        linkElement.appendChild(linkText);
         todoElement.appendChild(todoText);
+        todoElement.appendChild(linkElement);
         listElement.appendChild(todoElement);
+        
     }
 }
 
@@ -23,6 +32,11 @@ function addTodo() {
     var todoText = inputElement.value;
     todos.push(todoText);
     inputElement.value = '';
+    renderTodos();
+}
+
+function deleteTodo(pos){
+    todos.splice(pos, 1);
     renderTodos();
 }
 
